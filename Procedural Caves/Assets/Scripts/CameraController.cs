1ca988db25cb83287d6f78290213aa9123ee64d7
DraigLlofrudd <draig@lokink.com>
2015-09-07 21:14:54 +0100

    Fixed third person camera and added possibility to zoom in/out
    Things added are:
    -Fully functional 3rd person (finally!)
    -Controls relative to camera (I brute forced that a bit, but it works
    :satisfied: )
    -Zoom in and out. You can invert the zoom axis as well.

--- Procedural Caves/Assets/Scripts/CameraController.cs
+++ Procedural Caves/Assets/Scripts/CameraController.cs
@@ -12,12 +12,33 @@ public class CameraController : MonoBehaviour {
 	private float rotOffset;
 
 	public int lookSensitivity;
+	public int zoom3rdPersonSensitivity = 1;
+	public bool isZoomDirectionInverted = false;
+	private int zoomDirection = -1;
+	public int minZoomDistance = 1;
+	public int maxZoomDistance = 14;
 	private float cameraRotation;
+
+	public Transform target;
+
+	private float distance = 10.0f;
 	
+	private float xSpeed = 250.0f;
+	private float ySpeed = 120.0f;
+	
+	public int yMinLimit = -5;
+	public int yMaxLimit = 5;
+	
+	private float x = 0.0f;
+	private float y = 0.0f;
 	// Use this for initialization
 	void Start () {
 		offsetDistance = Mathf.Sqrt((transform.position - player.transform.position).sqrMagnitude);	// Watch out: The length of Magnitude is a square!
 		oldPlayerPos = player.transform.position;
+		
+		Vector3 angles = transform.eulerAngles;
+		x = angles.y;
+		y = angles.x;
 	}
 	
 	// LateUpdate is run just after Update!
@@ -27,13 +48,42 @@ public class CameraController : MonoBehaviour {
 		//offset.y = 0;
 		//cameraRotation = transform.rotation.eulerAngles.y;
 		rotOffset = Input.GetAxisRaw ("Mouse X") * lookSensitivity;
-		transform.RotateAround(playerPos, Vector3.up, rotOffset);
+		//transform.RotateAround(playerPos, Vector3.up, rotOffset);
 		//transform.rotation = Quaternion.Euler(Mathf.Acos(offset.x),Mathf.Acos(offset.y),Mathf.Acos(offset.z));
 		//if (playerPos != oldPlayerPos) {
 			//offset = (transform.position - player.transform.position).normalized;
 			//transform.position = playerPos + offset * offsetDistance;
 		//}
 		//oldPlayerPos = playerPos;
+
+
+		if (target) {
+			x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+			y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+			if (isZoomDirectionInverted) {
+				zoomDirection = -1;
+			} else { zoomDirection = 1; }
+			float zoom3rdPerson = Input.GetAxis("Mouse ScrollWheel") * zoomDirection;
+			distance += zoom3rdPerson * zoom3rdPersonSensitivity;
+			distance = Mathf.Clamp (distance, minZoomDistance, maxZoomDistance);
+
+			y = ClampAngle(y, yMinLimit, yMaxLimit);
+			
+			Quaternion rotation = Quaternion.Euler(y, x, 0);
+			Vector3 distanceVector = new Vector3(0, 0, -distance);
+			Vector3 position = rotation * distanceVector + target.position;
+			
+			transform.rotation = rotation;
+			transform.position = position;
+		}
+	}
+	
+	static float ClampAngle (float angle, float min, float max) {
+		if (angle < -360)
+			angle += 360;
+		if (angle > 360)
+			angle -= 360;
+		return Mathf.Clamp (angle, min, max);
 	}
 
 	/*
--- Procedural Caves/Assets/Scripts/PlayerController.cs
+++ Procedural Caves/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+	
+	public Transform cameraTransform;
+	Vector3 offset;
 
 	Rigidbody playerRigidbody;
 	Vector3 velocity;
@@ -13,7 +16,18 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		offset = transform.position - cameraTransform.position;
+		offset.y = 0;
+
 		velocity = new Vector3 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw("Jump"), Input.GetAxisRaw ("Vertical")).normalized * 10;
+
+		float offsetAngle = Vector3.Angle (Vector3.forward, offset);
+		float otherOffsetAngle = Vector3.Angle (Vector3.left, offset);
+
+		if (offsetAngle > otherOffsetAngle) {
+			offsetAngle = -offsetAngle;
+		}
+		velocity = Quaternion.Euler (0, offsetAngle, 0) * velocity;
 	}
 
 	void FixedUpdate () {
