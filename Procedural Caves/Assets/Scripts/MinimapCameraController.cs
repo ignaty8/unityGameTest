cc7ee49550d0cdef2ddf8294b2ff5556da2c4a64
DraigLlofrudd <draig@lokink.com>
2015-09-08 21:40:24 +0100

    Added Minimap
    Added a second orthographic camera that tracks the player from above and
    is rendered in the upper right corner. :)

---
+++ Procedural Caves/Assets/Scripts/MinimapCameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapCameraController : MonoBehaviour {
+
+	public Transform target;
+	private Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetPosition = new Vector3 (target.position.x, 144, target.position.z);
+		transform.position = targetPosition;
+	}
+}
