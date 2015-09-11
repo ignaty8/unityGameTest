0dbd632886753f98d54b5f598ff76be160e8409e
DraigLlofrudd <draig@lokink.com>
2015-09-11 03:30:41 +0100

    Just need to keep these changes somewhere
    The only reason I'm creating this branch, since there is no code to
    speak of yet!

---
+++ Procedural Caves/Assets/Scripts/HeightmapGenerator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeightmapGenerator : MonoBehaviour {
+
+	int[,] heightMap;
+	int size;
+	public int sizeMagnitude;
+
+	void Start(){
+		size = 2 ^ sizeMagnitude;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
