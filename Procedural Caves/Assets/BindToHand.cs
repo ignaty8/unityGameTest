b81f4dad3f8675d919bc88e25216ce38836293fb
DraigLlofrudd <draig@lokink.com>
2015-09-14 02:09:57 +0100

    Sensor-controlled hands and sword!
    So, after many a battle with the sdk and compiler, I have managed it!
    Well, kinda.
    
    Hands captured by the sensor follow your character, and stay (mostly)
    within your field of view.
    
    I've also managed to make a sword that (kinda) stays in your hand. It
    works fine when everything is on default, but when I increase the
    transform sensitivity for the tracking (i. e. moving my hand by 1 cm
    moves the virtual one by 4, or 8...), the sword follows a point a
    distance away from the hand...
    
    Will see what i can do about that later.

---
+++ Procedural Caves/Assets/BindToHand.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class BindToHand : MonoBehaviour {
+
+	
+	Controller controller = new Controller ();
+	GameObject handController, mainCamera;
+
+	public float movementScaleX, movementScaleY, movementScaleZ;
+	
+	// Use this for initialization
+	void Start () {
+		handController = GameObject.FindGameObjectWithTag ("HandController");
+		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		Frame frame = controller.Frame ();
+		HandList hands = frame.Hands;
+		foreach (Hand hand in hands) {
+			if(hand.IsValid){
+				if(hand.IsRight){
+					MoveToHand(hand.Id,frame);
+				}
+			}
+		}
+	}
+
+	void MoveToHand(int handID, Frame frame){
+		Hand targetHand = frame.Hand (handID);
+
+		//HandController handControllerScript = handController.GetComponent<HandController> ();
+		//handControllerScript.
+
+		Vector3 handPosition = targetHand.PalmPosition.ToUnityScaled ();
+		handPosition = new Vector3 (handPosition.x * movementScaleX, handPosition.y * movementScaleY, handPosition.z * movementScaleZ);
+			
+		//float distance = handPosition.magnitude;
+		//Vector3 distanceVector = new Vector3(0, 0, -distance);
+
+
+
+		//
+		Vector3 handDirection = targetHand.Direction.ToUnity();
+		transform.localRotation = Quaternion.LookRotation(handDirection, Vector3.Cross (targetHand.PalmNormal.ToUnity(), handDirection))// * mainCamera.transform.rotation;
+			;//
+
+		//transform.localRotation = transform.rotation * handController.transform.rotation; 
+
+		//Vector3 relativePosition = handController.transform.rotation * distanceVector + handController.transform.position;
+
+
+
+		transform.localPosition = handPosition// + handController.transform.position;
+		                                           ;
+	}
+}
---
+++ Procedural Caves/Assets/Scripts/Leap Motion/HandPinch.cs
@@ -0,0 +1,58 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//using Leap;
+//
+//public class HandPinch : MonoBehaviour
+//{
+//
+//
+//	void UpdatePinch (Frame frame)
+//	{
+//
+//		bool trigger_pinch = false;
+//
+//		Hand hand = frame.Hands [handIndex];
+//
+//			
+//			
+//		// Thumb tip is the pinch position.
+//				
+//		Vector3 thumb_tip = hand.Fingers [0].TipPosition.ToUnityScaled ();
+//
+//			
+//			
+//		// Check thumb tip distance to joints on all other fingers.
+//				
+//		// If it's close enough, start pinching.
+//				
+//		for (int i = 1; i < NUM_FINGERS && !trigger_pinch; ++i) {
+//
+//			Finger finger = hand.Fingers [i];
+//		
+//				
+//				
+//			for (int j = 0; j < NUM_JOINTS && !trigger_pinch; ++j) {
+//
+//				Vector3 joint_position = finger.JointPosition ((Finger.FingerJoint)(j)).ToUnityScaled ();
+//
+//				Vector3 distance = thumb_tip - joint_position;
+//
+//				if (distance.magnitude < THUMB_TRIGGER_DISTANCE)
+//						
+//					trigger_pinch = true;
+//
+//			}
+//
+//		}
+//
+//			
+//			
+//		// Only change state if it's different.
+//				
+//		if (trigger_pinch && !pinching_)
+//					
+//			OnPinch (pinch_position);
+//
+//	}
+//
+//}
